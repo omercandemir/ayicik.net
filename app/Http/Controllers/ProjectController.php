d1ea9e70d262dd7e7b581ff55b5564611d5b43e9
reatweb <reatweb@gmail.com>
2021-05-07 23:20:28 +0300

    project category relationship

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Settings;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        return "project page";
+        $settings = Settings::first();
+        $projects = Projects::with('projectCategory')->orderBy('id', 'desc')->paginate(8);
+        return view('project-list', [
+            'settings'  => $settings,
+            'projects'  => $projects,
+        ])
     }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ProjectCategory;
 
 class Project extends Model
 {
     use HasFactory;
+    public function projectCategory()
+    {
+        return $this->hasOne(ProjectCategory::class, 'id', 'category');
+    }
 }
